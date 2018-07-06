c63ba3aaaf2a3667b96c78e2a733e16103cd608d
Yamashita Satoshi <satoshi.yamashita1224@gmail.com>
2018-07-06 16:57:49 +0900

    webブラウザからインテントを受け取ってURLを取得

--- app/src/main/java/jp/ac/titech/itpro/sdl/cooknote/MainActivity.kt
+++ app/src/main/java/jp/ac/titech/itpro/sdl/cooknote/MainActivity.kt
@@ -1,12 +1,19 @@
 package jp.ac.titech.itpro.sdl.cooknote
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(Intent.ACTION_SEND == intent.action){
+            val uri = intent.extras.getCharSequence(Intent.EXTRA_TEXT).toString()
+            testText.text = uri
+        }
     }
 }
